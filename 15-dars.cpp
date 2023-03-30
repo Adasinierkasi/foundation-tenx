ea35dfc06dfdbfbb3910e51d2680038cc0639a90
sherbek.mavlonov <mavlonovsherbek43@gmail.com>
2023-03-30 14:57:42 +0500

    ADD. Ignore file

---
+++ 15-dars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// Berilgan matnda berilgan so'z necha marta ishtirok etganini topish
+
+// 
+// a = "Assalom alaykum Assalom"
+// b = "Assalom"
+// 2
+
+
+
+int main () {
+
+  return 0;
+}
