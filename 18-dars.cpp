a9af8b3b6232d254a3202a167769bd10d4087a38
sherbek.mavlonov <mavlonovsherbek43@gmail.com>
2023-04-04 17:23:01 +0500

    18-dars

---
+++ 18-dars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// qaytadiganqiymat functionNomi() {}
+
+int main(){
+  ECHO OFF
+  ECHO OPEN Explorer
+  PAUSE
+
+
+  return 0;
+}
+
+
+// https://www.geeksforgeeks.org/bitwise-operators-in-c-cpp/
