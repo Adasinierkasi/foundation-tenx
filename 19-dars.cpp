c99f3baf02c1c154f8198e21d4329e04617e1a78
sherbek.mavlonov <mavlonovsherbek43@gmail.com>
2023-04-05 21:00:39 +0500

    HomeWork 19

--- 19-dars.cpp
+++ 19-dars.cpp
@@ -2,27 +2,22 @@
 
 using namespace std;
 
-
-int main ()  {
-  // double onaTili[] =  {1,2,3,4};
-  // double matem[] =  {1,2,3,4};
-  // double tarix[] =  {1,2,3,4};
-  // double fizika[] =  {1,2,3,4};
-
-  double baholar[3][5] = {
-    {1,2,3,4},
-    {1,2,3,4},
-    {1,2,3,4}
-  };
-
-  for(int i = 0;i < 3; i++ ){
-    for(int j = 0; j < 5; j++){
-       cout << baholar[i][j] << " ";
+void bubbleSort(int arr[], int arrSize) {
+  for (int i = 0; i < arrSize-1; i++){
+    for (int j = 0; j < arrSize - 1; j++)
+    if(arr[j + 1] < arr[j]){
+      swap(arr[j], arr[j + 1]);
     }
-    cout << endl;
   }
 
+  for(int i = 0; i < arrSize; i++){
+    cout << arr[i] << " ";
+  }
+}
 
 
+int main ()  {
+  int arr[] = { 12, 321, 14, 54};
+  bubbleSort(arr,4);
   return 0;
 }
